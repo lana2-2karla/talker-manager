8c82c84417fddb621c2e120fec1f1a6863b8895f
Allana Karla <allanakarla1702@gmail.com>
2022-05-16 20:53:50 -0300

    :sparkles: feat: criação do endpoint post /login

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const route = require('express').Router();
 const express = require('express');
 const { readTalker } = require('../helpers');
+const generateToken = require('../utils/token');
 // const errorTalker = require('../middleware/errorMiddleware');
 
 route.use(express.json());
@@ -10,14 +11,17 @@ route.get('/talker', async (_req, rest) => {
     rest.status(200).json(talker);
 });
 
-// const error = { status: 400, message: 'erro' };
-
 route.get('/talker/:id', async (req, res) => {
     const { id } = req.params;
     const talker = await readTalker();
     const talkerId = talker.find((talk) => talk.id === +id);
     if (!talkerId) res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
     res.status(200).json(talkerId);
+});
+
+route.post('/login', (_req, res) => {
+const token = generateToken();
+res.status(200).json({ token });
 });
 
 module.exports = route;
