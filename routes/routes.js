4abb804b0dbd55f8292847f4b1c8f4573d2240c0
Allana Karla <allanakarla1702@gmail.com>
2022-05-19 12:59:08 -0300

    :sparkles: feat: criação da rota DELETE /talker:id

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const route = require('express').Router();
 const express = require('express');
-const { readTalker, writeTalker, editTalker } = require('../helpers');
+const { readTalker, writeTalker, editTalker, deleteTalker } = require('../helpers');
 const generateToken = require('../utils/token');
 const validation = require('../middlewares/loginMiddleware');
 const tokenMiddleware = require('../middlewares/tokenValidationMiddleware');
@@ -65,6 +65,12 @@ verifyTalkRate,
 verifyTalkWatchedAt, async (req, res) => {
 const talkersEdit = await editTalker(+req.params.id, req.body);
 return res.status(200).json(talkersEdit);
+});
+
+route.delete('/talker/:id',
+tokenMiddleware, async (req, res) => {
+await deleteTalker(+req.params.id);
+return res.status(204).json();
 });
 
 module.exports = route;
